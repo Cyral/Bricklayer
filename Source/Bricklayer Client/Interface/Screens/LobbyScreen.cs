0551320d0d40c06199b6a1aa41fe2e6b3298282a
Cyral <cyral@zarknorth.com>
2014-04-05 18:44:37 -0400

    Added base of the lobby window

---
+++ Source/Bricklayer Client/Interface/Screens/LobbyScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Microsoft.Xna.Framework;
+using TomShane.Neoforce.Controls;
+
+namespace Bricklayer.Client.Interface
+{
+    public class LobbyScreen : BaseScreen
+    {
+        public LobbyWindow Lobby;
+
+        public override void Add(ScreenManager screenManager)
+        {
+            base.Add(screenManager);
+            (Manager.Game as Application).BackgroundImage = ContentPack.Textures["gui\\background"];
+            //Add the login window
+            Lobby = new LobbyWindow(Manager);
+            Lobby.Init();
+            Window.Add(Lobby);
+            Lobby.Show();
+        }
+        public override void Remove()
+        {
+            Window.Remove(Lobby);
+            (Manager.Game as Application).BackgroundImage = null;
+        }
+    }
+}
---
+++ Source/Bricklayer Client/Interface/Windows/LobbyWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Bricklayer.Client.Networking;
+using TomShane.Neoforce.Controls;
+
+namespace Bricklayer.Client.Interface
+{
+    /// <summary>
+    /// A small window containing the lobby room list and server data
+    /// </summary>
+    public class LobbyWindow : Dialog
+    {
+        public LobbyWindow(Manager manager)
+            : base(manager)
+        {
+            //Setup the window
+            CaptionVisible = false;
+            TopPanel.Visible = false;
+            Movable = false;
+            Resizable = false;
+            Width = 700;
+            Height = 500;
+            Shadow = true;
+            Center();
+            
+        }
+    }
+}
