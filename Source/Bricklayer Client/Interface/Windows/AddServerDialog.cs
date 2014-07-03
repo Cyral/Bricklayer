469eb776005f1681631b397f7d8f7d0019dde299
Cyral <cyral@zarknorth.com>
2014-07-02 21:31:49 -0400

    Added validation to Add/Edit server window

--- Source/Bricklayer Client/Interface/Windows/AddServerDialog.cs
+++ Source/Bricklayer Client/Interface/Windows/AddServerDialog.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using Bricklayer.Client.Networking;
 using Bricklayer.Common.Data;
@@ -21,6 +22,9 @@ namespace Bricklayer.Client.Interface
         private TextBox NameTxt, AddressTxt;
         private Label NameLbl, AddressLbl;
         private ServerWindow ServerList;
+        private string IP;
+        private int port;
+        private char[] separator = new char[1] { ':' };
 
         public AddServerDialog(Manager manager, ServerWindow parent, int index, bool edit, string name, string address)
             : base(manager)
@@ -45,6 +49,7 @@ namespace Bricklayer.Client.Interface
             NameTxt = new TextBox(manager) { Left = 8, Top = NameLbl.Bottom + 4, Width = this.ClientWidth - 16 };
             NameTxt.Init();
             NameTxt.Text = name;
+            NameTxt.TextChanged += NameTxt_TextChanged;
             Add(NameTxt);
 
             AddressLbl = new Label(manager) { Left = 8, Top = NameTxt.Bottom + 8, Text = "Address: (Default port is 14242)", Width = this.ClientWidth - 16 };
@@ -54,41 +59,65 @@ namespace Bricklayer.Client.Interface
             AddressTxt = new TextBox(manager) { Left = 8, Top = AddressLbl.Bottom + 4, Width = this.ClientWidth - 16 };
             AddressTxt.Init();
             AddressTxt.Text = address;
+            AddressTxt.TextChanged += AddressTxt_TextChanged;
             Add(AddressTxt);
 
             SaveBtn = new Button(manager) { Top = 8, Text = Edit ? "Save" : "Add", };
             SaveBtn.Init();
             SaveBtn.Left = (Width / 2) - (SaveBtn.Width / 2);
             SaveBtn.Click += SaveBtn_Click;
+            SaveBtn.Enabled = false;
             BottomPanel.Add(SaveBtn);
         }
-        /// <summary>
-        /// When the saved button is clicked
-        /// </summary>
-        void SaveBtn_Click(object sender, TomShane.Neoforce.Controls.EventArgs e)
+
+        void NameTxt_TextChanged(object sender, TomShane.Neoforce.Controls.EventArgs e)
         {
-            if (Edit)
+            SaveBtn.Enabled = Validate();
+        }
+
+        private void AddressTxt_TextChanged(object sender, TomShane.Neoforce.Controls.EventArgs e)
+        {
+            SaveBtn.Enabled = Validate();
+        }
+
+        private bool Validate()
+        {
+            //Validate name
+            if (string.IsNullOrWhiteSpace(NameTxt.Text))
+                return false;
+            string[] address = AddressTxt.Text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            port = 0;
+            if (address.Length > 0)
             {
-                //TODO: Add validation and error handling
-                string[] address = AddressTxt.Text.Split(':');
-                string IP;
-                int port = 0;
                 IP = address[0];
+                //Validate IP
+                IPAddress host;
+                if (!IPAddress.TryParse(IP, out host))
+                    return false;
+                //Validate port (if any)
                 if (address.Length > 1)
+                {
                     port = int.Parse(address[1]);
-                ServerList.EditServer(Index, new ServerSaveData(NameTxt.Text, IP, port));
+                    if (!(port > 0 && port < 65535))
+                        return false;
+                }
             }
             else
             {
-                //TODO: Add validation and error handling
-                string[] address = AddressTxt.Text.Split(':');
-                string IP;
-                int port = 0;
-                IP = address[0];
-                if (address.Length > 1)
-                    port = int.Parse(address[1]);
-                ServerList.AddServer(new ServerSaveData(NameTxt.Text, IP, port));
+                return false;
             }
+            return true; //Passed checks
+        }
+
+        /// <summary>
+        /// When the save button is clicked
+        /// </summary>
+        private void SaveBtn_Click(object sender, TomShane.Neoforce.Controls.EventArgs e)
+        {
+            if (Edit)
+                ServerList.EditServer(Index, new ServerSaveData(NameTxt.Text, IP, port));
+            else
+                ServerList.AddServer(new ServerSaveData(NameTxt.Text, IP, port));
             Close();
         }
     }
